8a70e16c32b2f8b70e7a675a66698dc5c5305f40
Jinjae Lee <jinjaelee@Administrators-MacBook-Pro-681.local>
2020-02-15 16:09:05 +0900

    json testing 2

--- script.js
+++ script.js
@@ -2,26 +2,28 @@ function loadJSON(callback) {
 
     var xobj = new XMLHttpRequest();
     xobj.overrideMimeType("application/json");
-    xobj.open('GET', 'test.json', true); // Replace 'my_data' with the path to your file
-    xobj.onreadystatechange = function () {
+    xobj.open('GET', 'test.json', true);
+    xobj.onreadystatechange = function() {
         if (xobj.readyState == 4 && xobj.status == "200") {
-            // Required use of an anonymous callback as .open will NOT return a value but simply returns undefined in asynchronous mode
+
+            // .open will NOT return a value but simply returns undefined in async mode so use a callback
             callback(xobj.responseText);
+
         }
-    };
+    }
     xobj.send(null);
+
 }
 
-function init() {
-    loadJSON(function (response) {
-        // Parse JSON string into object
-        var actual_JSON = JSON.parse(response);
-        console.log(actual_JSON)
-    });
-}
+// Call to function with anonymous callback
+loadJSON(function(response) {
+    // Do Something with the response e.g.
+    jsonresponse = JSON.parse(response);
 
+    // Assuming json data is wrapped in square brackets as Drew suggests
+    console.log(jsonresponse[0].name);
 
-init()
+});
 
 // Seperate array to json file
 var testArray = [{
